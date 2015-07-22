270fbded9bd93a6c75df138cc0656f4267b8ca58
tanveerm176 <tanveerm176@gmail.com>
2015-07-22 11:39:46 -0400

    added extract funcs to repo

---
+++ extract.py
@@ -0,0 +1,39 @@
+import urllib2 
+import json as js
+
+def extract_labels():
+    galax_url =
+    urllib2.urlopen('http://star-api.herokuapp.com/api/v1/local_groups')
+    galax_info = galax_url.read()
+    galax_params = js.loads(galax_info)
+    label= []
+    
+    for i in galax_params:
+         label.append(i['label'])
+    
+    return label
+
+
+def extract_distly():
+    galax_url =
+    urllib2.urlopen('http://star-api.herokuapp.com/api/v1/local_groups')
+    galax_info = galax_url.read()
+    galax_params = js.loads(galax_info)
+    distance = []
+    
+    for i in galax_params:
+        distance.append(i['distly'])
+    
+    return distance
+    
+
+
+
+# def galax_dict():
+#     label_list = extract.extract[0] 
+#     dist_list = extract.extract[1]
+    
+    
+
+
+
