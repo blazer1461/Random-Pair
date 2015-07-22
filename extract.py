bd95188aac9c2cdcca7d65b6333e1d4fff87f778
tanveerm176 <tanveerm176@gmail.com>
2015-07-22 13:32:32 -0400

    finished extract functions

--- extract.py
+++ extract.py
@@ -2,8 +2,7 @@ import urllib2
 import json as js
 
 def extract_labels():
-    galax_url =
-    urllib2.urlopen('http://star-api.herokuapp.com/api/v1/local_groups')
+    galax_url = urllib2.urlopen('http://star-api.herokuapp.com/api/v1/local_groups')
     galax_info = galax_url.read()
     galax_params = js.loads(galax_info)
     label= []
@@ -15,8 +14,7 @@ def extract_labels():
 
 
 def extract_distly():
-    galax_url =
-    urllib2.urlopen('http://star-api.herokuapp.com/api/v1/local_groups')
+    galax_url = urllib2.urlopen('http://star-api.herokuapp.com/api/v1/local_groups')
     galax_info = galax_url.read()
     galax_params = js.loads(galax_info)
     distance = []
@@ -29,11 +27,18 @@ def extract_distly():
 
 
 
-# def galax_dict():
-#     label_list = extract.extract[0] 
-#     dist_list = extract.extract[1]
-    
-    
+label_list = extract_labels() 
+dist_list = extract_distly()
+galax_dict = {}
 
+def extract_dict():
+    len_label = range(len(label_list))
+    
+    for x in len_label:
+        galax_dict[label_list[x]]=(dist_list[x])
+    
+    return galax_dict
 
+def select_galax(galaxy):
+    return extract_dict()[galaxy]
 
