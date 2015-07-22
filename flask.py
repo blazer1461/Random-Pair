d5d409c456fba2438b7023e8dc44f5d47b50d9d4
Your Name <you@example.com>
2015-07-22 10:56:05 -0400

    PLs

--- flask.py
+++ flask.py
@@ -6,8 +6,12 @@ census= Flask (__name__)
 @census.route("/", methods=["POST", "GET"])
 
 def census_taker():
-
-    d= API.extract(tracking)
+    if request.method == "GET":
+        return render_template("base.html", galaxy= "Nothing yet", label= label)
+    elif request.method == "POST":
+        tracking= request.form["Select_galaxies"]
+        d= API.extract(tracking)
+        return render_template("base.html", galaxy= label, answer= d)
 
 
 
