bb4f6b35c81de424f28213457d85ff18eb4d020b
Your Name <you@example.com>
2015-07-23 09:29:49 -0400

    234

--- apiflask.py
+++ apiflask.py
@@ -12,9 +12,12 @@ def census_taker():
         return render_template("base.html", galaxy= "Nothing yet", label= temp)
     elif request.method == "POST":
         tracking= request.form["Select_galaxies"]
+        aging= request.form["age"]
         d= extract.select_galax(tracking)
         e= time_calc.time(float(d))
-        return render_template("answer.html", galaxy= tracking, answer= float(d), label= temp, answer2= e)
+        total_time= e - float(aging)
+
+        return render_template("answer.html", galaxy= tracking, answer= float(d), label= temp, answer2= e, age= total_time)
 
 if __name__ == "__main__":
 
